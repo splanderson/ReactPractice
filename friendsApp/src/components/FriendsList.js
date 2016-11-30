b5309dcb3863d11d118abd2c2121fcc8ee04469c
Eric Anderson <splanderson@gmail.com>
2016-11-29 20:05:55 -0700

    added friends app

---
+++ friendsApp/src/components/FriendsList.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import friends from '../../friends'
+import Friend from './Friend';
+
+const friendsList = friends
+    .filter( friend => friend.name.toLowerCase().indexOf( this.state.searchText.toLowerCase() ) !== -1 )
+    .sort( ( a, b ) => a[ this.state.orderBy ] > b[ this.state.orderBy ] )
+    .map( friend => (
+        <Friend
+            currentLocation={ friend.current_location || {} }
+            friendCount={ friend.friend_count }
+            key={ friend.$$hashKey }
+            name={ friend.name }
+            pictureUrl={ friend.pic_square }
+            status={ friend.status ? friend.status.message : "" }
+        />
+    ) );
+
+const displayFriends = this.state.order === "ascending" ? friendsList : friendsList.slice().reverse();
+
+
+class FriendsList extends React.Component {
+  render() {
+      const friendsList = friends.map( friend => (
+          <Friend
+              currentLocation={ friend.current_location || {} }
+              friendCount={ friend.friend_count }
+              key={ friend.$$hashKey }
+              name={ friend.name }
+              pictureUrl={ friend.pic_square }
+              status={ friend.status ? friend.status.message : "" }
+          />
+      ) );
+
+      return (
+          // ...
+          <ul>
+              { friendsList }
+          </ul>
+      );
+  }
+  constructor( props ) {
+      super( props );
+
+      this.state = {
+            searchText: ""
+          , orderBy: "name"
+          , order: "ascending"
+      };
+  }
+  handleChange( field, event ) {
+     this.setState( { [ field ]: event.target.value } );
+ }
+  render() {
+    return (
+      <div>
+          <form
+            className="form-inline searchForm"
+            role="form"
+            >
+              <div className="form-group">
+
+                  <input
+                    className="form-control"
+                    onChange={this.handleChange.bind(this, "searchText")}
+                    placeholder="Search For Friends"
+                    value={this.state.searchText}
+                  />
+                  <select
+                    className="input-medium"
+                    onChange={this.handleChange.bind(this, "orderBy")}
+                    value={this.state.orderBy}
+                  >
+                      <option value="name">Name</option>
+                      <option value="friend_count">#Friends</option>
+                  </select>
+
+                  <select
+                    className="input-medium"
+                    onChange={this.handleChange.bind(this, "order")}
+
+                    value={this.state.order}
+                  >
+                      <option value="descending">Descending</option>
+                      <option value="ascending">Ascending</option>
+                  </select>
+
+              </div>
+          </form>
+            <ul>
+            </ul>
+      </div>
+    );
+  }
+}
+
+export default FriendsList;
